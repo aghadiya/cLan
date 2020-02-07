3bac05de3a67889b269fed843752ada2b3c4164c
aghadiya <aghadiya@users.noreply.github.com>
2020-02-07 13:15:46 -0500

    ds-stack- verified
    Verified stack functionality

--- ds/stack/test_stack.c
+++ ds/stack/test_stack.c
@@ -6,6 +6,7 @@ int main (void)
     STACK_RET eRet = STACK_PASS;
     int i;
     int inv_push;
+	int pop_data = 0;
     struct stack oStack;
     /* printf("\n\rSTACK> Stack Verification!!!"); */
     oStack.top = NULL;
@@ -14,6 +15,10 @@ int main (void)
     for(i = 1; i <= STACK_SIZE; i++)
     {
         eRet |= stack_push(i, &oStack);
+		if(eRet == STACK_PASS)
+		{
+			printf("\n\rSTACK> Pushed %d",i);
+		}
     }
     if(eRet != STACK_PASS)
     {
@@ -32,6 +37,34 @@ int main (void)
     else
     {
         printf("\n\rSTACK> Fail: Failed to report full.");
+    }
+	
+	eRet = STACK_PASS;
+    for(i = 1; i <= STACK_SIZE; i++)
+    {
+        eRet |= stack_pop(&pop_data, &oStack);
+		if(eRet == STACK_PASS)
+		{
+			printf("\n\rSTACK> Popped %d",pop_data);
+		}
+    }
+    if(eRet != STACK_PASS)
+    {
+        printf("\n\rSTACK> Pop failed.");
+    }
+    else
+    {
+        printf("\n\rSTACK> Pop passed.");
+    }
+
+    eRet = stack_pop(&pop_data, &oStack);
+    if(eRet == STACK_EMPTY)
+    {
+        printf("\n\rSTACK> Pass: Reported Empty.");
+    }
+    else
+    {
+        printf("\n\rSTACK> Fail: Failed to report empty.");
     }
 
 return(0);
