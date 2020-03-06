40bfe8ef12466c9dbda1763b3ea95c7b0881cf1d
aghadiya <aghadiya@users.noreply.github.com>
2020-03-05 22:53:46 -0500

    Updated stack and queue
    init api is added for stack and queue

---
+++ ds/queue/inc/queue.h
@@ -0,0 +1,34 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define QUEUE_SIZE		(10u)
+
+struct node
+{
+	int data;
+	struct node *link;
+};
+
+struct queue
+{
+	struct node *front;
+	struct node *rear;
+	unsigned int num_of_nodes;
+	unsigned int size;
+};
+
+typedef enum
+{
+	QUEUE_PASS,
+	QUEUE_EMPTY,
+	QUEUE_FULL,
+}QUEUE_RET;
+
+/*Queue api prototypes. */
+QUEUE_RET queue_init(struct queue *pQueue, unsigned int nSize);
+QUEUE_RET enqueue(int data, struct queue *pQueue);
+QUEUE_RET dequeue(int *pData, struct queue *pQueue);
+#endif /* QUEUE_H */
