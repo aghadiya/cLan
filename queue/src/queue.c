23bcd685a521f2071ec5c17ee550e5e71d434bb9
aghadiya <aghadiya@users.noreply.github.com>
2020-03-05 22:22:48 -0500

    added queue code
    queue implementation is added

---
+++ queue/src/queue.c
@@ -0,0 +1,67 @@
+#include "queue.h"
+
+QUEUE_RET queue_init(struct queue *pQueue, int nSize)
+{
+	pQueue->front = NULL;
+	pQueue->rear = NULL;
+	pQueue->num_of_nodes = 0;
+	pQueue->size = nSize;
+return(QUEUE_PASS);
+}
+
+QUEUE_RET enqueue(int data, struct queue *pQueue)
+{
+	QUEUE_RET eRet = QUEUE_PASS;
+	struct node *temp;
+	
+	if(QUEUE_SIZE <= pQueue->num_of_nodes)
+	{
+		eRet = QUEUE_FULL;
+	}
+	else
+	{
+		temp = malloc (sizeof(struct node));
+	}
+	
+	if(eRet == QUEUE_PASS)
+	{
+		temp->data = data;
+		temp->link = NULL;
+		pQueue->num_of_nodes++;
+		if(pQueue->front == NULL)
+		{
+			pQueue->front = temp;
+			pQueue->rear = temp;
+		}
+		else
+		{
+			pQueue->rear->link = temp;
+			pQueue->rear = temp;
+		}
+	}
+return(eRet);
+}
+
+QUEUE_RET dequeue(int *pData, struct queue *pQueue)
+{
+	QUEUE_RET eRet = QUEUE_PASS;
+	struct node *temp;
+
+	if(pQueue->num_of_nodes == 0)
+	{
+		eRet = QUEUE_EMPTY;
+	}
+	else
+	{
+		pQueue->num_of_nodes--;
+	}
+	if(eRet == QUEUE_PASS)
+	{
+		temp = pQueue->front;
+		pQueue->front = pQueue->front->link;
+		*pData = temp->data;
+		/*free(temp);*/
+	}
+return(eRet);
+}
+
---
+++ queue/src/test_queue.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include "queue.h"
+int main (void)
+{
+	QUEUE_RET eRet = QUEUE_PASS;
+	struct queue oQueue;
+	int i, inv_enqueue;
+	int dequeued_data;
+	printf("\n\rQUEUE> Queue verification start!!!");
+
+	eRet = queue_init(&oQueue, QUEUE_SIZE);
+	if(QUEUE_PASS != eRet)
+	{
+		printf("\n\rQUEUE> Queue initialization failed.");
+	}
+	else
+	{
+		printf("\n\rQUEUE> Queue initialization passed.");
+	}
+	
+	for(i = 1; i <= oQueue.size; i++)
+	{
+		eRet |= enqueue(i, &oQueue);
+		if(eRet == QUEUE_PASS)
+		{
+			printf("\n\rQUEUE> Enqueued %d", i);
+		}
+	}
+	inv_enqueue = oQueue.size + 1;
+	eRet = enqueue(inv_enqueue, &oQueue);
+	if(eRet == QUEUE_FULL)
+	{
+		printf("\n\rQUEUE> Reported overflow.");
+	}
+	else
+	{
+		printf("\n\rQUEUE> Failed to report overflow.");
+	}
+	
+	eRet = QUEUE_PASS;
+	for(i = 0; i < oQueue.size; i++)
+	{
+		eRet |= dequeue(&dequeued_data, &oQueue);
+		if(eRet == QUEUE_PASS)
+		{
+			printf("\n\rQUEUE> Dequeued %d", dequeued_data);
+		}
+	}
+
+	eRet = dequeue(&dequeued_data, &oQueue);
+	if(eRet == QUEUE_EMPTY)
+	{
+		printf("\n\rQUEUE> Reported underflow.");
+	}
+	else
+	{
+		printf("\n\rQUEUE> Failed to report underflow.");
+	}
+
+
+return(0);
+}
+
